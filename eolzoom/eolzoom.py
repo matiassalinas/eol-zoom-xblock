0c50ca1c5015ed35cc94e8ce6e0f60209f3ef744
matsalinas <matias.salinas.s@mail.pucv.cl>
2020-03-12 19:36:11 +0000

    Author view

--- eolzoom/eolzoom.py
+++ eolzoom/eolzoom.py
@@ -49,6 +49,7 @@ class EolZoomXBlock(StudioEditableXBlockMixin, XBlock):
     )
 
     editable_fields = ('display_name', 'url', 'date', 'time', 'description',)
+    has_author_view = True
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
@@ -64,10 +65,19 @@ class EolZoomXBlock(StudioEditableXBlockMixin, XBlock):
         frag.initialize_js('EolZoomXBlock')
         return frag
 
+    def author_view(self, context=None):
+        context_html = self.get_context()
+        template = self.render_template('static/html/author_view.html', context_html)
+        frag = Fragment(template)
+        frag.add_css(self.resource_string("static/css/eolzoom.css"))
+        return frag
+
     def get_context(self):
+        status = self.get_status() # Status: false (at least one attribute is empty), true (all attributes have content)
         return {
             'xblock': self,
-            'zoom_logo_path' : self.runtime.local_resource_url(self, "static/images/ZoomLogo.png")
+            'zoom_logo_path' : self.runtime.local_resource_url(self, "static/images/ZoomLogo.png"),
+            'status' : status
         }
 
     def render_template(self, template_path, context):
@@ -75,6 +85,18 @@ class EolZoomXBlock(StudioEditableXBlockMixin, XBlock):
         template = Template(template_str)
         return template.render(Context(context))
 
+    def get_status(self):
+        """
+            Return false if at least one attribute is empty
+        """
+        return not (
+            is_empty(self.display_name) or
+            is_empty(self.url) or
+            is_empty(self.date) or
+            is_empty(self.time) or
+            is_empty(self.description)
+        )
+    
     @staticmethod
     def workbench_scenarios():
         """A canned scenario for display in the workbench."""
@@ -83,3 +105,8 @@ class EolZoomXBlock(StudioEditableXBlockMixin, XBlock):
              """<eolzoom/>
              """),
         ]
+def is_empty(attr):
+    """
+        check if attribute is empty or None
+    """
+    return attr == "" or attr == None

277643d1146f9e5131d2bb055f22cfe66ec6a73f
Luis Santana <luis.santana101@gmail.com>
2022-12-02 13:05:10 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,9 +32,9 @@ setup(
     ],
     install_requires=[
         'XBlock',
-        "google-api-python-client>=1.10.0",
-        "google-auth<2.0dev,>=1.25.0",
-        "google-auth-httplib2>=0.0.3",
+        "google-api-python-client==1.10.0",
+        "google-auth<2.0dev,==1.25.0",
+        "google-auth-httplib2==0.0.3",
         "google-auth-oauthlib==0.4.1"],
     entry_points={
         'xblock.v1': [
